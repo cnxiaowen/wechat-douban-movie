c49a462d92daedae129c45377f8249cdb7195cfd
Cwen <Chun5en@QQ.com>
2018-03-19 22:25:38 +0800

    20180319 add poster page.

---
+++ pages/movies/poster/poster.js
@@ -0,0 +1,75 @@
+// pages/movies/poster/poster.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    title: "奇迹男孩 Wonder",
+    images: {
+      large: "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2507709428.jpg"
+    },
+    rating: {
+      average: 8.6,
+      max: 10,
+      min: 0,
+      stars: "45"
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
