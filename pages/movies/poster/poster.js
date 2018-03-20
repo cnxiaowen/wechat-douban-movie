3c89908f2877b5ae867ac029d63d1171b1a2e893
Cwen <Chun5en@QQ.com>
2018-03-20 20:54:10 +0800

    20180320 add block page.

--- pages/movies/poster/poster.js
+++ pages/movies/poster/poster.js
@@ -5,16 +5,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-    title: "奇迹男孩 Wonder",
-    images: {
-      large: "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2507709428.jpg"
-    },
-    rating: {
-      average: 8.6,
-      max: 10,
-      min: 0,
-      stars: "45"
-    }
+    // title: "奇迹男孩 Wonder",
+    // images: {
+    //   large: "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2507709428.jpg"
+    // },
+    // rating: {
+    //   average: 8.6,
+    //   max: 10,
+    //   min: 0,
+    //   stars: "45"
+    // }
   },
 
   /**
